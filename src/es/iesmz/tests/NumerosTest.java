36723499200be23ff069960ece7098d309d72b31
OmarLlinares <llinares.omar@gmail.com>
2024-03-25 09:35:26 +0100

    2º commit
    Primer test

--- src/es/iesmz/tests/NumerosTest.java
+++ src/es/iesmz/tests/NumerosTest.java
@@ -3,4 +3,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumerosTest {
+
+    @Test
+    public void sonIguales1(){
+        assertEquals(2, Numeros.sonIguales(4, 55, 7));
+    }
+
+
 }
